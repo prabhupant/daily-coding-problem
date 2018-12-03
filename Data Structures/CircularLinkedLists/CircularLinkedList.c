9aeb6d85d66f13c762f0003304adb1a1863812bf
Prabhu Pant <prabhupant09@gmail.com>
2018-12-03 17:47:29 +0530

    Add circular linked list and its basic operations

---
+++ Data Structures/CircularLinkedLists/CircularLinkedList.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+void addToEmpty(struct Node **last_ref, int data)
+{
+    if(*last_ref != NULL)
+    {
+        printf("The list is not empty!");
+        return;
+    }
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = data;
+    new_node->next = NULL;
+    *last_ref = new_node;
+    (*last_ref)->next = *last_ref;
+}
+
+void insertFront(struct Node **last_ref, int data)
+{
+    if(*last_ref == NULL)
+    {
+        addToEmpty(&*last_ref, data);
+        return;
+    }
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = data;
+    new_node->next = (*last_ref)->next;
+    (*last_ref)->next = new_node;
+    return;
+}
+
+void insertAfter(struct Node** last, int data, int item)
+{
+    if(last == NULL)
+        printf("The list is empty!");
+    struct Node *new_node, *curr;
+    curr = (*last)->next;
+    do
+    {
+        if(curr->data == item)
+        {
+            new_node = (struct Node*)malloc(sizeof(struct Node));
+            new_node->data = data;
+            new_node->next = curr->next;
+            curr->next = new_node;
+
+            if(curr == *last)
+            {
+                *last = new_node;
+            }
+            return;
+        }
+        curr = curr->next;
+    }while(curr != (*last)->next);
+    printf("The node is not present in the list!");
+}
+
+void insertEnd(struct Node **last, int data)
+{
+    if(last == NULL)
+        return addToEmpty(&*last, data);
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node*));
+    new_node->data = data;
+    new_node->next = (*last)->next;
+    (*last)->next = new_node;
+    *last = new_node;
+}
+
+void printList(struct Node *last)
+{
+    struct Node* curr;
+    if(last == NULL)
+    {
+        printf("The list is empty!");
+        return;
+    }
+    curr = last->next;
+    do
+    {
+        printf("%d \n", curr->data);
+        curr = curr->next;
+    }while(curr != last->next);
+}
+
+int main()
+{
+    struct Node* last = NULL;
+    addToEmpty(&last, 3);
+    insertFront(&last, 2);
+    insertFront(&last, 1);
+    insertEnd(&last, 4);
+    insertEnd(&last, 6);
+    insertAfter(&last, 5, 4);
+    printList(last);
+    return 0;
+}
+
+
